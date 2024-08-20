9c4477398383df4c0f9cfb06196d80f9cecc056c
@mmcgregor <mmcgregor@deft.com>
2024-08-20 16:06:06 -0500

    updated formatting helper to include habit id and habit days id

--- server/utils/habitControllerHelpers.js
+++ server/utils/habitControllerHelpers.js
@@ -1,12 +1,17 @@
 const shapeHabitsWithDaysForReact = (habitWithDaysData) => {
     const result = {};
     let habitName = null;
+    let habitId = null;
     
     habitWithDaysData.forEach(day => {
-        const { date, completed, week, habitName: name } = day;
+        const { date, completed, week, habitName: name, habitId: id, habitDayId } = day;
 
         if (!habitName) {
             habitName = name;
+        }
+        
+        if (!habitId) {
+            habitId = id;
         }
         
         const formattedWeek = new Date(week).toISOString().split('T')[0];
@@ -16,7 +21,10 @@ const shapeHabitsWithDaysForReact = (habitWithDaysData) => {
             result[formattedWeek] = {};
         }
 
-        result[formattedWeek][formattedDate] = completed;
+        result[formattedWeek][formattedDate] = {
+            completed,
+            habitDayId: habitDayId || null
+        };
     });
     
     // Convert result object to desired format
@@ -26,10 +34,12 @@ const shapeHabitsWithDaysForReact = (habitWithDaysData) => {
         const days = Array(7).fill(null).map((_, index) => {
             const currentDay = new Date(startOfWeek.getTime() + index * 24 * 60 * 60 * 1000);
             const dateStr = currentDay.toISOString().split('T')[0]; // Format date as YYYY-MM-DD. Might be a better way to do this.
-
+            const dayData = result[weekStartDate][dateStr] || { completed: 0, habitDayId: null };
+            
             return {
                 date: dateStr,
-                completed: result[weekStartDate][dateStr] || 0 // Default to 0/incomplete if not found. This keeps the week as 7 days long
+                completed: dayData.completed, // Default to 0/incomplete if not found. This keeps the week as 7 days long
+                habitDayId: dayData.habitDayId
             };
         });
 
@@ -39,7 +49,7 @@ const shapeHabitsWithDaysForReact = (habitWithDaysData) => {
         };
     }).filter(Boolean);
     
-    return {habitName, weeks};
+    return {habitName, habitId, weeks};
 }
 
 module.exports = {
