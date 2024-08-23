571cf9abf28aaf6b09b652add6985293cef53e78
@mmcgregor <mmcgregor@deft.com>
2024-08-23 16:11:51 -0500

    google oauth functionality is working on the most fundemental level. ok to revert back here if you blow shit up

--- client/src/containers/LandingPage.js
+++ client/src/containers/LandingPage.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import LoginForm from '../components/Login/LoginForm';
 import CalendarPage from './CalendarPage';
+import { GoogleLogin } from '@react-oauth/google';
+import '../styling/LandingPage.css';
+
 
 const LandingPage = () => {
 
@@ -10,14 +13,22 @@ const LandingPage = () => {
         setIsUserLoggedIn(!isUserLoggedIn);
     }
 
+    const responseMessage = (credentialResponse) => {
+        console.log("Google Login Successful: ", credentialResponse);
+    };
+
+    const errorMessage = (error) => {
+        console.error("Google Login Error: ", error);
+    }
     return (
-        <div>
+        <div className='landing-page-container'>
             {isUserLoggedIn ? (
                 <CalendarPage handleLogin={handleLogin} />
             ) : (
                 <>
                     <h1>Welcome to Seinfeld Cal ❌</h1>
                     <LoginForm handleLogin={handleLogin} />
+                    <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
                 </>
             )}
         </div>
