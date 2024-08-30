3749f621be0be41ee63ba6c4a398a83066ebf3e1
@mmcgregor <mmcgregor@deft.com>
2024-08-30 16:10:26 -0500

    wrote endpoint to retrieve all of a user's habits with days, shaped so they can easily be passed through the calendar display components

--- server/controllers/habitsController.js
+++ server/controllers/habitsController.js
@@ -43,3 +43,34 @@ exports.getHabitWithDaysByHabitId = async (req, res) => {
         res.status(500).json({ error: `There was an error retrieving habit days. Err: ${err.message}` });
     }
 }
+
+exports.getUserHabitsWithDaysByUserId = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const query = 'SELECT h.id as habitId, h.start_date as habitStartDate, hd.id as habitDayId, h.name AS habitName, h.start_date AS startDate, hd.date, hd.completed, hd.week FROM habits h JOIN habit_days hd ON h.id = hd.habit_id WHERE h.user_id = ? ORDER BY hd.date';
+        const [rows] = await pool.query(query, [userId]);
+        
+        if (rows.length === 0) {
+            return res.status(200).json({ message: `User #${userId} does not have any habits yet.` });
+        }
+        
+        // Group rows by habitId
+        const habitsGroupedByHabitId = rows.reduce((acc, row) => {
+            if (!acc[row.habitId]) {
+                acc[row.habitId] = [];
+            }
+            acc[row.habitId].push(row);
+            return acc;
+        }, {});
+
+        //Reshape data for each habit and return an array of reshaped habits
+        const reshapedHabits = Object.values(habitsGroupedByHabitId).map(habitRows =>
+            reshapeData.shapeHabitsWithDaysForReact(habitRows)
+        );
+        
+        res.status(200).json(reshapedHabits);
+        
+    } catch (err) {
+        res.status(500).json({ error: `There was an error retrieving habit days for the logged in user. Err: ${err.message}` });
+    }
+}
--- server/routes/habitRoutes.js
+++ server/routes/habitRoutes.js
@@ -5,5 +5,6 @@ const habitsController = require('../controllers/habitsController');
 router.get('/all_habits', habitsController.getHabits);
 router.get('/:id', habitsController.getHabitById);
 router.get('/habit_with_days/:habitId', habitsController.getHabitWithDaysByHabitId);
+router.get('/user/:userId/habits_with_days/', habitsController.getUserHabitsWithDaysByUserId);
 
 module.exports = router;
