dadd470a31de93dd26d3a8a47bf07b7e60a31a89
@mmcgregor <mmcgregor@deft.com>
2024-09-11 17:03:59 -0500

    styled buttons on login modal

--- client/src/components/Login/LoginModal.js
+++ client/src/components/Login/LoginModal.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Modal from 'react-bootstrap/Modal';
 import GoogleAuth from "../Authentication/GoogleAuth";
 import '../../styling/LoginPage.css';
@@ -6,24 +6,31 @@ import AboutAppModal from "../Modals/AboutAppModal";
 import Button from "react-bootstrap/button";
 
 const LoginModal = ({ setUser, setToken }) => {
-    
+
     const [showAboutAppModal, setShowAboutAppModal] = useState(false);
     const [showLoginModal, setShowLoginModal] = useState(true);
 
     return (
         <>
-        <Modal show={showLoginModal} centered={true} className="login-modal-custom">
+            <Modal show={showLoginModal} centered={true} className="login-modal-custom">
                 <Modal.Body>
-                    <Button variant="outline-secondary" onClick={() => {
-                        setShowAboutAppModal(true) 
-                        setShowLoginModal(false)
-                    } }>About App</Button><Button variant="outline-secondary" href="https://maxmcgregor.com" target="_blank">About the Creator</Button>
-                <h2>Welcome to Seinfeld Calendar</h2>
-                <p>To create your own trackers, login with Google</p>
-                <div className="d-flex justify-content-center">
-                    <GoogleAuth setUser={setUser} setToken={setToken} />
-                </div>
-            </Modal.Body>
+                    <div className="button-container">
+                        <Button variant="secondary" onClick={() => {
+                            setShowAboutAppModal(true)
+                            setShowLoginModal(false)
+                        }}>
+                            About the App
+                        </Button>
+                        <Button variant="secondary" href="https://maxmcgregor.com" target="_blank">
+                            About the Creator
+                        </Button>
+                    </div>
+                    <h2>Welcome to Seinfeld Calendar</h2>
+                    <p>To create your own trackers, login with Google</p>
+                    <div className="d-flex justify-content-center">
+                        <GoogleAuth setUser={setUser} setToken={setToken} />
+                    </div>
+                </Modal.Body>
             </Modal>
             <AboutAppModal
                 showModal={showAboutAppModal}
