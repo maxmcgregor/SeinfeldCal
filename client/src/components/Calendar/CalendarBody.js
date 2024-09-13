5769bd3efc8b895d175b42e5439f0b6cda46e43e
@mmcgregor <mmcgregor@deft.com>
2024-09-13 14:19:54 -0500

    tweaked styling of calendar weeks to be cleaner, don't go all the way to the bottom, fade on top and bottom, scroll defaults to bottom for long running habits

--- client/src/components/Calendar/CalendarBody.js
+++ client/src/components/Calendar/CalendarBody.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import CalendarWeek from './CalendarWeek';
 import { FormatDate } from '../../utils/CalendarHelpers';
+import '../../styling/CalendarDay.css';
 import Button from 'react-bootstrap/Button';
 
 
@@ -8,6 +9,14 @@ const CalendarBody = ({ calendarData }) => {
 
     const { weeks, habitStartDate } = calendarData;
     const formattedHabitStartDate = FormatDate(habitStartDate, 'long', 'numeric', 'numeric', true, true, true);
+    
+    const scrollRef = useRef(null);
+    
+    useEffect(() => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+        }
+    }, [weeks]);
 
     const devButton = () => {
         console.log("weeks: ", weeks);
@@ -19,7 +28,7 @@ const CalendarBody = ({ calendarData }) => {
         <>
             {/* format this dynamically like you do the title. on a big enough screen it's kind of fucked up */}
             <p className="sticky-date-started-header">Habit started {formattedHabitStartDate}</p> 
-            <div className="scrollable-calendar-weeks">
+            <div className="scrollable-calendar-weeks" ref={scrollRef}>
             {weeks.map((weekData, index) => (
                 <CalendarWeek
                     key={weekData.weekStartDate}
